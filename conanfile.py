dda648338211b76716be8054474ba04b61f4ccaf
Kartik <kartik.aiyer@gmail.com>
2025-07-20 22:36:42 -0700

    First Commit

---
+++ conanfile.py
@@ -0,0 +1,14 @@
+from conan import ConanFile
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+class CampiConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("spdlog/1.9.2")
+        self.requires("uvw/3.4.0")
+
+    def layout(self):
+        cmake_layout(self)
