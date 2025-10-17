528f88bb51320b4f4ea559680b4bea8215d68c06
Kartik <kartik.aiyer@gmail.com>
2025-10-17 09:32:51 -0700

    Remove spdlog since its not used

--- conanfile.py
+++ conanfile.py
@@ -6,8 +6,5 @@ class CampiConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
 
-    def requirements(self):
-        self.requires("spdlog/1.9.2")
-
     def layout(self):
         cmake_layout(self)
